7eeff3ad7d4a50dcc6d2265c588ebe0a3fac0c2c
Orlando Valdez <or.valdez@duocuc.cl>
2023-05-25 00:08:48 +0000

    Comienzo de clase de listas en python

---
+++ 24-05-2023_Listas_.py
@@ -0,0 +1,17 @@
+lista = [1,2,3,4]
+listaDentroDeLista = [1,2,3,4,["one","two","three"]]
+
+def suma(x,y):
+    return x+y
+
+
+x = 4
+y = 1
+
+resultado = suma(x,y)
+print(resultado)
+
+print()
+
+for i in lista:
+    print(i)
