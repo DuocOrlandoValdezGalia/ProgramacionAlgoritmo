6e40a14d322f90211045ca11fc01028ece9fe9ab
Your Name <you@example.com>
2023-05-31 20:04:17 -0400

    Programa que cuenta palabras

---
+++ MenuConFunciones2.py
@@ -0,0 +1,19 @@
+import os
+
+def ContadorPalabras(texto):
+    palabras = texto.split()
+    return len(palabras)
+
+
+texto = input("Ingrese un texto: ")
+resultado = ContadorPalabras(texto)
+print(f"El texto contiene {resultado} palabras")
+
+
+n = 1
+
+if n == 1:
+    nombre = 2
+    os.system
+else:
+    nombre = 2
